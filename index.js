aea46b3ac1d3187640d39af970657e67f01e02fd
Dyalpa <117543340+Dyalpa@users.noreply.github.com>
2025-01-05 02:38:10 -0500

    mejoramiento del código Sqlite

--- index.js
+++ index.js
@@ -12,22 +12,15 @@ db.on('connect', () => {
   console.log('Conectado a la base de datos SQLite en la nube.');
 });
 
-db.on('error', (err) => {
-  console.error('Error de conexión:', err.message);
-});
-
 // Ruta para obtener todos los datos de cto sin filtro
 app.get('/cto', (req, res) => {
   const query = 'SELECT * FROM cto';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+           res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -35,51 +28,38 @@ app.get('/cto', (req, res) => {
 // Ruta para obtener datos de cto filtrado por `numero_cto`
 app.get('/cto-buscar', (req, res) => {
   const cto = req.query.cto;
-  console.log('Valor recibido para cto:', cto);
 
   if (!cto) {
     res.status(400).send('El parámetro cto es requerido.');
     return;
   }
 
-  const query = `SELECT * FROM cto WHERE numero_cto = "${cto}"`;
-  console.log('Consulta SQL:', query);
-
+  const query = `SELECT * FROM cto WHERE numero_cto = "${cto}"`;  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
-
 
 // Ruta para actualizar la dirección de una CTO
 app.put('/cto/:id/direccion', (req, res) => {
   const ctoId = req.params.id;
   const nuevaDireccion = req.body.direccion;
-  // Verificar si los parámetros se reciben correctamente
-  console.log('Actualizando CTO ID:', ctoId);
-  console.log('Nueva dirección:', nuevaDireccion);
 
   if (!ctoId || !nuevaDireccion) {
     res.status(400).send('ID de CTO y nueva dirección son requeridos.');
     return;
   }
-  // Concatenar parámetros directamente en la consulta
+  
   const query = `UPDATE cto SET direccion_cto = '${nuevaDireccion}' WHERE numero_cto = '${ctoId}'`;
-  console.log('Consulta SQL:', query);
-
-  db.run(query, function(error) {
+    db.run(query, function(error) {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send('Error ejecutando query: ' + error.message);
+            res.status(500).send('Error ejecutando query: ' + error.message);
     } else {
-      console.log('Número de cambios realizados:', this.changes);
-      res.status(200).json({ changes: this.changes });
+            res.status(200).json({ changes: this.changes });
     }
   });
 });
@@ -87,15 +67,12 @@ app.put('/cto/:id/direccion', (req, res) => {
 // Ruta para obtener todos los datos de abonado sin filtro
 app.get('/abonado', (req, res) => {
   const query = 'SELECT * FROM abonado';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+           res.json(results);
     }
   });
 });
@@ -103,23 +80,18 @@ app.get('/abonado', (req, res) => {
 // Ruta para obtener datos de abonado filtrados por `abonado`
 app.get('/abonado-buscar', (req, res) => {
   const abonado = req.query.abonado;
-  console.log('Valor recibido para abonado:', abonado);
-
+  
   if (!abonado) {
     res.status(400).send('El parámetro abonado es requerido.');
     return;
   }
 
   const query = `SELECT * FROM abonado WHERE abonado = "${abonado}"`;
-  console.log('Consulta SQL:', query);
-
-  db.all(query, [], (error, results) => {
+    db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -127,15 +99,12 @@ app.get('/abonado-buscar', (req, res) => {
 // Ruta para obtener todos los datos de cai sin filtro
 app.get('/cai', (req, res) => {
   const query = 'SELECT * FROM cai';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -143,23 +112,18 @@ app.get('/cai', (req, res) => {
 // Ruta para obtener datos de cai filtrados por `cai`
 app.get('/cai-buscar', (req, res) => {
   const cai = req.query.cai;
-  console.log('Valor recibido para cai:', cai);
-
+  
   if (!cai) {
     res.status(400).send('El parámetro cai es requerido.');
     return;
   }
 
   const query = `SELECT * FROM cai WHERE CAI = "${cai}"`;
-  console.log('Consulta SQL:', query);
-
-  db.all(query, [], (error, results) => {
+   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -167,15 +131,12 @@ app.get('/cai-buscar', (req, res) => {
 // Ruta para obtener todos los datos de cliente sin filtro
 app.get('/cliente', (req, res) => {
   const query = 'SELECT * FROM cliente';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -183,23 +144,18 @@ app.get('/cliente', (req, res) => {
 // Ruta para obtener datos de cliente filtrados por `cedula`
 app.get('/cliente-buscar', (req, res) => {
   const cedula = req.query.cedula;
-  console.log('Valor recibido para cliente:', cedula);
-
+  
   if (!cedula) {
     res.status(400).send('El parámetro cedula es requerido.');
     return;
   }
 
   const query = `SELECT * FROM cliente WHERE cedula = "${cedula}"`;
-  console.log('Consulta SQL:', query);
-
-  db.all(query, [], (error, results) => {
+    db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -246,14 +202,10 @@ app.get('/historial/tecnico-productos', (req, res) => {
   `;
   
   if (fecha_actuacion) {
-    console.log('Filtrando por fecha:', fecha_actuacion);
-    query += ` AND DATE(fecha_actuacion) = "${fecha_actuacion}"`;
+        query += ` AND DATE(fecha_actuacion) = "${fecha_actuacion}"`;
   }
 
   query += ' GROUP BY producto';
-
-  console.log('Consulta SQL:', query);
-
   db.all(query, [], (error, productResults) => {
     if (error) {
       res.status(500).send(error.message);
@@ -267,8 +219,6 @@ app.get('/historial/tecnico-productos', (req, res) => {
         totalQuery += ` AND DATE(fecha_actuacion) = "${fecha_actuacion}"`;
       }
 
-      console.log('Consulta Total SQL:', totalQuery);
-
       db.get(totalQuery, [], (error, totalResults) => {
         if (error) {
           res.status(500).send(error.message);
@@ -279,10 +229,6 @@ app.get('/historial/tecnico-productos', (req, res) => {
     }
   });
 });
-
-
-
-
 
 // Ruta para obtener datos de spliter agrupados por CTO y contados por cantidad para una CTO en específico
 app.get('/spliter/spliter-CTO', (req, res) => {
@@ -317,15 +263,12 @@ app.get('/spliter/spliter-CTO', (req, res) => {
 // Ruta para obtener todos los datos de tecnicos sin filtro
 app.get('/tecnico', (req, res) => {
   const query = 'SELECT * FROM tecnico';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -333,23 +276,18 @@ app.get('/tecnico', (req, res) => {
 // Ruta para obtener datos de técnicos filtrado por `cedula`
 app.get('/tecnico-buscar', (req, res) => {
   const cedula = req.query.cedula;
-  console.log('Valor recibido para tecnico:', cedula);
-
+  
   if (!cedula) {
     res.status(400).send('El parámetro cedula es requerido.');
     return;
   }
 
   const query = `SELECT * FROM tecnico WHERE cedula = "${cedula}"`;
-  console.log('Consulta SQL:', query);
-
-  db.all(query, [], (error, results) => {
+    db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -357,15 +295,12 @@ app.get('/tecnico-buscar', (req, res) => {
 // Ruta para obtener todos los datos de spliter sin filtro
 app.get('/spliter', (req, res) => {
   const query = 'SELECT * FROM spliter';
-  console.log('Consulta SQL:', query);
-
+  
   db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
+            res.json(results);
     }
   });
 });
@@ -373,54 +308,18 @@ app.get('/spliter', (req, res) => {
 // Ruta para obtener datos de spliter filtrado por `spliter`
 app.get('/spliter-buscar', (req, res) => {
   const spliter = req.query.spliter;
-  console.log('Valor recibido para spliter:', spliter);
-
+  
   if (!spliter) {
     res.status(400).send('El parámetro spliter es requerido.');
     return;
   }
 
   const query = `SELECT * FROM spliter WHERE spliter = "${spliter}"`;
-  console.log('Consulta SQL:', query);
-
-  db.all(query, [], (error, results) => {
+    db.all(query, [], (error, results) => {
     if (error) {
-      console.error('Error ejecutando query:', error.message);
-      res.status(500).send(error.message);
+            res.status(500).send(error.message);
     } else {
-      console.log('Resultados de la consulta:', results);
-      res.json(results);
-    }
-  });
-});
-
-// Rutas para las otras tablas...
-app.get('/cai', (req, res) => {
-  db.all('SELECT * FROM cai', (error, results) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.get('/cliente', (req, res) => {
-  db.all('SELECT * FROM cliente', (error, results) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.get('/tecnico', (req, res) => {
-  db.all('SELECT * FROM tecnico', (error, results) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else {
-      res.json(results);
+            res.json(results);
     }
   });
 });
